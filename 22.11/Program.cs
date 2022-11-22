f8bf15f5bffd5d2c0cc4ae67ed3fb21e8791fc64
alest1448 <118421044+alest1448@users.noreply.github.com>
2022-11-22 13:55:37 +0900

    Initial commit

---
+++ 22.11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите 2 числа");
+                int x = Convert.ToInt32(Console.ReadLine());
+                int y = Convert.ToInt32(Console.ReadLine());
+                int z = x / y;
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            }
+            finally
+            {
+                Console.WriteLine("------------");
+            }
+            Console.WriteLine("Конец программы");
+        }
+
+    }
+}
